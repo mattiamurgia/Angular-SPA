ba2f3fcfd1c2b67e0aeeed37f051a71ed92973b7
giauw.sk <gianvito.scandurra@stevejobs.academy>
2023-06-13 01:08:19 +0200

    Revert "Merge branch 'main' of https://github.com/mattiamurgia/Angular-SPA"
    This reverts commit f8fbb4eedf656923ed3424c6db70e493f55fa53b, reversing
    changes made to 8569cd0ddbee99d8d7485f8312848d40106f81bd.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,19 +7,8 @@ import { LocalService } from 'src/app/services/localService/local-service.servic
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-
 export class NavbarComponent {
-  /* Questa parte la utilizziamo per far comparire o scomparire lìimput al click del bottone di ricerca */
-  showInput = false;
-  searchQuery!: string;
 
-  toggleInput() {
-    this.showInput = !this.showInput;
-    if (!this.showInput) {
-      this.searchQuery = ''; // Resettiamo la query di ricerca quando viene nascosta l'input
-    }
-  }
-/* ------------------------------------------------------------------------------------------------------ */
   service = inject(LocalService);
 
   userName = JSON.stringify(this.service.getUser());
